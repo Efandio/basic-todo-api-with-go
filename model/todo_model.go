1ea8f0b918e429f43be6b0cbec3941c79d29188e
Efandio <efandio904@gmail.com>
2025-07-26 19:56:14 +0700

    user model & gofmt

--- model/todo_model.go
+++ model/todo_model.go
@@ -3,10 +3,10 @@ package model
 import "time"
 
 type Todo struct {
-	ID       string	`json:"id"`
-	Title    string	`json:"title"`
-	Complete bool	`json:"complete"`
-	CreateAt time.Time	`json:"create_at"`
-	UpdateAt time.Time	`json:"update_at"`
-	UserID	string	`json:"user_id"`
+	ID       string    `json:"id"`
+	Title    string    `json:"title"`
+	Complete bool      `json:"complete"`
+	CreateAt time.Time `json:"create_at"`
+	UpdateAt time.Time `json:"update_at"`
+	UserID   string    `json:"user_id"`
 }
