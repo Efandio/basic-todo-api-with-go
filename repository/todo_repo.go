c7aedfd314eb499211950810f7f9be9144a474bb
Efandio <efandio904@gmail.com>
2025-07-27 15:47:23 +0700

    added delete method

--- repository/todo_repo.go
+++ repository/todo_repo.go
@@ -4,7 +4,8 @@ import "todo-api/model"
 
 type TodoRepository interface {
 	Create(todo *model.Todo) (*model.Todo, error)
-	GetAllTodo(userID string) ([]model.Todo, error)
-	GetTodoById(userID, todoID string) (*model.Todo, error)
-	UpdateTodo(todo *model.Todo) error
+	GetAll(userID string) ([]model.Todo, error)
+	GetById(userID, todoID string) (*model.Todo, error)
+	Update(todo *model.Todo) error
+	Delete(todoID string) error
 }
