76f987dc0f4731b375c7ca77bb9946ee4d16b981
Efandio <efandio904@gmail.com>
2025-07-28 22:49:18 +0700

    constructor

--- service/todo_service.go
+++ service/todo_service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"database/sql"
 	"errors"
 	"time"
 	"todo-api/model"
@@ -10,22 +11,50 @@ import (
 )
 
 type TodoRepoImpl struct {
-	Repo *repository.TodoRepository
+	DB *sql.DB
 }
 
+
+func NewTodoRepoImpl(db *sql.DB) repository.TodoRepository {
+	return &TodoRepoImpl{
+		DB: db,
+	}
+}
+
+// Create implements repository.TodoRepository.
 func (t *TodoRepoImpl) Create(title, userID string) (*model.Todo, error) {
 	if title == "" {
 		return nil, errors.New("title cannot be empty")
 	}
 
 	todo := &model.Todo{
-		ID: uuid.NewString(),
-		Title: title,
+		ID:       uuid.NewString(),
+		Title:    title,
 		Complete: false,
 		CreateAt: time.Now(),
 		UpdateAt: time.Now(),
-		UserID: userID,
+		UserID:   userID,
 	}
 
 	return todo, nil
+}
+
+// Delete implements repository.TodoRepository.
+func (t *TodoRepoImpl) Delete(todoID string) error {
+	panic("unimplemented")
+}
+
+// GetAll implements repository.TodoRepository.
+func (t *TodoRepoImpl) GetAll(userID string) ([]model.Todo, error) {
+	panic("unimplemented")
+}
+
+// GetById implements repository.TodoRepository.
+func (t *TodoRepoImpl) GetById(userID string, todoID string) (*model.Todo, error) {
+	panic("unimplemented")
+}
+
+// Update implements repository.TodoRepository.
+func (t *TodoRepoImpl) Update(todo *model.Todo) error {
+	panic("unimplemented")
 }
